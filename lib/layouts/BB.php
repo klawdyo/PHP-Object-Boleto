045a7c6a820b50e6278426e2e9f16ce9d1f09b69
Claudio Medeiros <klawdyo@gmail.com>
2011-05-20 16:51:00 -0300

    Conserto de comentarios; Acrescimo de dados no layout BB.php

--- lib/layouts/BB.php
+++ lib/layouts/BB.php
@@ -1,5 +1,7 @@
 <?php
 class BB extends Layouts{
     public $nomebanco = 'Banco do Brasil';
+    public $logobanco = 'logobb.jpg';
+    public $css = 'bb.css';
     
 }
--- lib/layouts/Layouts.php
+++ lib/layouts/Layouts.php
@@ -7,6 +7,12 @@ class Layouts{
     public $css = 'default';//css utilizado no boleto desse banco
     public $template = 'default';
     
+    /*
+        @var array $relacoes
+        Armazena a relação entre os mais diversos bancos e os nomes dos
+        seus arquivos. Os nomes devem estar iguais, inclusive em relação
+        ao caso.
+     */
     public $relacoes = array(
         '001' => 'BB',
         '104' => 'Caixa',
@@ -15,14 +21,19 @@ class Layouts{
     
     
     /*
-    Pegando os dados do boleto constantes no código de barras
+        @var array $posicoes
+        Armazena os dados de posições dos valores dentro do código de barras.
+        
+        @example
+        O Fator de Vencimento (FatorVencimento) inicia na posição
+        "5" (contando a partir do zero), e tem 4 caracteres: (5,4)
     */
     public $posicoes = array(
                      //(Inicio, Tamanho). Inicia em 0
         'Banco'         => array(0,3),    //identificação do banco
         'Moeda'         => array(3,1),    //Código da moeda: real=9
         'DV'            => array(4,1),    //Dígito verificador geral da linha digitável
-        'FatorVencimento'=> array(5,4),    //Fator de vencimento (Dias passados desde 7/out/1997)
+        'FatorVencimento'=>array(5,4),    //Fator de vencimento (Dias passados desde 7/out/1997)
         'Valor'         => array(9,10),  //Valor nominal do título
         'Agencia'       => array(19,4),  //Código da agencia, sem dígito
         'Carteira'      => array(23,2),  //Código da Carteira
@@ -30,18 +41,21 @@ class Layouts{
         'Conta'         => array(36,7),  //Conta corrente do cedente, sem o dígito
     );
     
-    /* @var $layoutCodigoBarras
+    /*
+        @var $layoutCodigoBarras
         armazena o layout que será usado para gerar o código de barras desse banco
      */
     public $layoutCodigoBarras = ':Banco:Moeda:FatorVencimento:Valor:Agencia:Carteira:NossoNumero:Conta0';
     
-    /* @var $layoutCodigoBarras
+    /*
+        @var $layoutCodigoBarras
         Armazena o layout que será usado para gerar a linha digitável nesse banco
      */
     public $layoutLinhaDigitavel = ':Banco:Moeda:Agencia:Carteira:NossoNumero:Conta:FatorVencimento:Valor';
 
-    /* @var $formataLinhaDigitavel
-       Máscara para a linha digitável
+    /*
+        @var $formataLinhaDigitavel
+        Máscara para a linha digitável
      */
     public $mascaraLinhaDigitavel = '00000.00000 00000.000000 00000.000000 0 00000000000000';
     
