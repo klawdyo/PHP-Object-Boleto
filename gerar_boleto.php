080401cf629ec0bdb355615defbd0c2ea671c4b9
Claudio Medeiros <klawdyo@gmail.com>
2011-05-22 23:59:02 -0300

    Estilo da caixa adicionado

--- gerar_boleto.php
+++ gerar_boleto.php
@@ -6,7 +6,7 @@
     $ob = new OB();
     
     $ob->Vendedor
-            ->setBanco('104')
+            ->setBanco('001')
             ->setAgencia('214')
             ->setConta('10571')
             ->setNome('Jose Claudio Medeiros de Lima')
